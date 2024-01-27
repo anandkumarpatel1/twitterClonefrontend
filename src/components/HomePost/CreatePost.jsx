f44e04372ee2deec5a81e62f13f4fcbff3d15ef1
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-01-28 04:57:26 +0530

    .

--- src/components/HomePost/CreatePost.jsx
+++ src/components/HomePost/CreatePost.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./HomePost.scss";
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="createPost">
+      <div>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/1/17/Mangekyou_Sharingan_Itachi.svg"
+          alt="user"
+        />
+      </div>
+      <div>
+        <div>
+          <textarea placeholder="What is happing?!"></textarea>
+        </div>
+        <button>Post</button>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
--- src/components/HomePost/HomePost.jsx
+++ src/components/HomePost/HomePost.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./HomePost.scss";
 import UpperHeader from "./UpperHeader";
+import CreatePost from "./CreatePost";
 
 const HomePost = () => {
   return (
     <div className="homePost">
       <UpperHeader />
+      <CreatePost />
     </div>
   );
 };
