87edf9fd00367d242c12e928c5de7f7da571efd1
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-02-05 08:42:57 +0530

    .

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import SideBar from "../../components/SideBar/SideBar";
 import "./Profile.scss"
 import ProfileMain from "../../components/ProfileMain/ProfileMain";
+import HomeLeft from "../../components/HomeLeft/HomeLeft";
 
 const Profile = () => {
   return (
     <div className="profile">
       <SideBar />
       <ProfileMain />
+      <HomeLeft />
     </div>
   );
 };
