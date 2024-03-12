26d87cad383e3146e8cf88fb0bdf72c512aa64ae
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-03-13 00:07:54 +0530

    .

--- src/components/HomePost/AllPosts.jsx
+++ src/components/HomePost/AllPosts.jsx
@@ -1,26 +1,54 @@
 import React from "react";
 import "./HomePost.scss";
 import { SlOptions } from "react-icons/sl";
+import { useNavigate } from "react-router-dom";
 import { BiMessageRounded } from "react-icons/bi";
 import { FaRetweet } from "react-icons/fa6";
 import { FaRegHeart } from "react-icons/fa";
 import { ImParagraphRight } from "react-icons/im";
+import axios from "axios";
+import { UserState } from "../../context/context";
+import { toast } from "react-toastify";
 
 const AllPosts = ({ name, username, avatar, userId, desc, postImg }) => {
+  const navigate = useNavigate();
+  const { setLoading, setIdUser } = UserState();
+
+  const userHandler = async () => {
+    try {
+      setLoading(true);
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${document.cookie.split("=")[1]}`,
+        },
+        withCredentials: true,
+        sameSite: "None",
+      };
+
+      const { data } = await axios.get(
+        `https://backendtwitter.vercel.app/api/v1/users/user/${userId}`,
+        config
+      );
+
+      if (data) {
+        setIdUser(data?.user);
+      }
+      navigate(`/user/${userId}`);
+      setLoading(false);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="allPost">
-      {
-        console.log(name)
-      }
       <div>
-        <img
-          src={avatar}
-          alt={name}
-        />
+        <img src={avatar} alt={name} onClick={userHandler} />
       </div>
       <div>
         <div>
-          <div>
+          <div onClick={userHandler}>
             <p>{name}</p>
             <p>@{username}</p>
           </div>
