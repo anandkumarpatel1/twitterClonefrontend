04c46e87033f66ccac9d410b01aa1a0d7d1c4ab5
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-03-13 08:05:16 +0530

    .

--- src/components/Login/LoginModel.jsx
+++ src/components/Login/LoginModel.jsx
@@ -37,7 +37,7 @@ const LoginModel = ({ setLoginModel }) => {
       );
 
       if (data) {
-        document.cookie = `token=${data?.token}; max-age= 8,64,000`;
+        document.cookie = `token=${data?.token}; max-age= 864000`;
         setUser(data?.user);
         setChn(!chn);
         toast.success(data?.message);
